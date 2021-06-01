e729244cd4776becdc254184fd29aa71b2ec78cf
Radi Rusadi <radi.rusadi.s@gmail.com>
2021-06-01 23:16:44 +0700

    Refactored section widget

--- lib/screens/setting/setting_ui.screen.dart
+++ lib/screens/setting/setting_ui.screen.dart
@@ -12,61 +12,77 @@ class SettingScreen extends StatelessWidget {
         padding: EdgeInsets.all(8),
         child: Column(
           children: [
-            UiText(text: 'Asset Widget'),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            _buildSection(
+              'Asset Widget',
               children: [
-                ImgIcon(
-                  ImageIcons.camera,
-                  size: 30,
-                ),
-                LoadingIndicator()
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ImgIcon(
+                      ImageIcons.camera,
+                      size: 30,
+                    ),
+                    LoadingIndicator()
+                  ],
+                )
               ],
             ),
             VSpace.lg,
-            Container(
-              // color: Colors.grey,
-              padding: EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                // color: C,
-                borderRadius: BorderRadius.circular(8.0),
-                border: Border.all(
-                  width: 1,
-                  color: Colors.blueGrey,
-                  style: BorderStyle.solid,
+            _buildSection(
+              'Form Widget',
+              children: [
+                LabeledTextInput(
+                  label: 'Input Your Name',
                 ),
-              ),
-              child: Column(
-                children: [
-                  UiText(text: 'Form Widget'),
-                  LabeledTextInput(
-                    label: 'Input Your Name',
-                  ),
-                  VSpace.lg,
-                  LabeledTextInput(
-                    label: 'Input Your password',
-                    obscureText: true,
-                  ),
-                  VSpace.lg,
-                  Button(
+                VSpace.lg,
+                LabeledTextInput(
+                  label: 'Input Your password',
+                  obscureText: true,
+                ),
+                VSpace.lg,
+                FractionallySizedBox(
+                  widthFactor: 0.5,
+                  child: Button(
                     type: ButtonType.Elevated,
                     label: "Login",
                     onPressed: () => null,
                   ),
-                  Button(
-                    type: ButtonType.Outline,
-                    label: "Register",
-                    onPressed: () => null,
-                  ),
-                  Button(
-                    label: "Forgot Password",
-                    onPressed: () => null,
-                  ),
-                ],
-              ),
-            )
+                ),
+                Button(
+                  type: ButtonType.Elevated,
+                  label: 'Disabled',
+                ),
+                Button(
+                  type: ButtonType.Outline,
+                  label: "Register",
+                  onPressed: () => null,
+                ),
+                Button(
+                  label: "Forgot Password",
+                  onPressed: () => null,
+                ),
+              ],
+            ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildSection(String title, {required List<Widget> children}) {
+    return Container(
+      padding: EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        // color: C,
+        borderRadius: BorderRadius.circular(8.0),
+        border: Border.all(
+          width: 1,
+          color: Colors.blueGrey,
+          style: BorderStyle.solid,
+        ),
+      ),
+      child: Column(
+        children: [UiText(text: title), ...children.toList()],
       ),
     );
   }
--- lib/theme/styles.dart
+++ lib/theme/styles.dart
@@ -59,16 +59,18 @@ class Strokes {
 class Shadows {
   static List<BoxShadow> get universal => [
         BoxShadow(
-            color: Color(0xff333333).withOpacity(.15),
-            spreadRadius: 0,
-            blurRadius: 10),
+          color: Color(0xff333333).withOpacity(.15),
+          spreadRadius: 0,
+          blurRadius: 10,
+        ),
       ];
   static List<BoxShadow> get small => [
         BoxShadow(
-            color: Color(0xff333333).withOpacity(.15),
-            spreadRadius: 0,
-            blurRadius: 3,
-            offset: Offset(0, 1)),
+          color: Color(0xff333333).withOpacity(.15),
+          spreadRadius: 0,
+          blurRadius: 3,
+          offset: Offset(0, 1),
+        ),
       ];
 }
 
@@ -99,36 +101,71 @@ class Fonts {
 class TextStyles {
   /// Declare a base style for each Family
   static const TextStyle raleway = const TextStyle(
-      fontFamily: Fonts.raleway, fontWeight: FontWeight.w400, height: 1);
+    fontFamily: Fonts.raleway,
+    fontWeight: FontWeight.w400,
+    height: 1,
+  );
   static const TextStyle fraunces = const TextStyle(
-      fontFamily: Fonts.fraunces, fontWeight: FontWeight.w400, height: 1);
+    fontFamily: Fonts.fraunces,
+    fontWeight: FontWeight.w400,
+    height: 1,
+  );
 
   static TextStyle get h1 => fraunces.copyWith(
-      fontWeight: FontWeight.w600,
-      fontSize: FontSizes.s48,
-      letterSpacing: -1,
-      height: 1.17);
-  static TextStyle get h2 =>
-      h1.copyWith(fontSize: FontSizes.s24, letterSpacing: -.5, height: 1.16);
-  static TextStyle get h3 =>
-      h1.copyWith(fontSize: FontSizes.s14, letterSpacing: -.05, height: 1.29);
+        fontWeight: FontWeight.w600,
+        fontSize: FontSizes.s48,
+        letterSpacing: -1,
+        height: 1.17,
+      );
+  static TextStyle get h2 => h1.copyWith(
+        fontSize: FontSizes.s24,
+        letterSpacing: -.5,
+        height: 1.16,
+      );
+  static TextStyle get h3 => h1.copyWith(
+        fontSize: FontSizes.s14,
+        letterSpacing: -.05,
+        height: 1.29,
+      );
   static TextStyle get title1 => raleway.copyWith(
-      fontWeight: FontWeight.bold, fontSize: FontSizes.s16, height: 1.31);
+        fontWeight: FontWeight.bold,
+        fontSize: FontSizes.s16,
+        height: 1.31,
+      );
   static TextStyle get title2 => title1.copyWith(
-      fontWeight: FontWeight.w500, fontSize: FontSizes.s14, height: 1.36);
+        fontWeight: FontWeight.w500,
+        fontSize: FontSizes.s14,
+        height: 1.36,
+      );
   static TextStyle get body1 => raleway.copyWith(
-      fontWeight: FontWeight.normal, fontSize: FontSizes.s14, height: 1.71);
-  static TextStyle get body2 =>
-      body1.copyWith(fontSize: FontSizes.s12, height: 1.5, letterSpacing: .2);
+        fontWeight: FontWeight.normal,
+        fontSize: FontSizes.s14,
+        height: 1.71,
+      );
+  static TextStyle get body2 => body1.copyWith(
+        fontSize: FontSizes.s12,
+        height: 1.5,
+        letterSpacing: .2,
+      );
   static TextStyle get body3 => body1.copyWith(
-      fontSize: FontSizes.s12, height: 1.5, fontWeight: FontWeight.bold);
+        fontSize: FontSizes.s12,
+        height: 1.5,
+        fontWeight: FontWeight.bold,
+      );
   static TextStyle get callout1 => raleway.copyWith(
-      fontWeight: FontWeight.w800,
-      fontSize: FontSizes.s12,
-      height: 1.17,
-      letterSpacing: .5);
-  static TextStyle get callout2 =>
-      callout1.copyWith(fontSize: FontSizes.s10, height: 1, letterSpacing: .25);
+        fontWeight: FontWeight.w800,
+        fontSize: FontSizes.s12,
+        height: 1.17,
+        letterSpacing: .5,
+      );
+  static TextStyle get callout2 => callout1.copyWith(
+        fontSize: FontSizes.s10,
+        height: 1,
+        letterSpacing: .25,
+      );
   static TextStyle get caption => raleway.copyWith(
-      fontWeight: FontWeight.w500, fontSize: FontSizes.s11, height: 1.36);
+        fontWeight: FontWeight.w500,
+        fontSize: FontSizes.s11,
+        height: 1.36,
+      );
 }
