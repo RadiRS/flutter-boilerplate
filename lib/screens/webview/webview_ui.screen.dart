bcb665907eb0d5caad0a02509d0b7ed96aceaf4f
Radi Rusadi <radi.rusadi.s@gmail.com>
2021-06-11 15:28:41 +0700

    Added progress indicator at webview screen

--- lib/screens/webview/webview_ui.screen.dart
+++ lib/screens/webview/webview_ui.screen.dart
@@ -4,10 +4,17 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class WebviewScreen extends StatelessWidget {
+class WebviewScreen extends StatefulWidget {
   static String routeName = '/webview';
 
   const WebviewScreen({Key? key}) : super(key: key);
+
+  @override
+  _WebviewScreenState createState() => _WebviewScreenState();
+}
+
+class _WebviewScreenState extends State<WebviewScreen> {
+  double _progress = 0;
 
   JavascriptChannel _toasterJavascriptChannel(BuildContext context) {
     return JavascriptChannel(
@@ -35,34 +42,45 @@ class WebviewScreen extends StatelessWidget {
         ],
       ),
       body: SafeArea(
-        child: WebView(
-          initialUrl: 'https://flutter.dev',
-          javascriptMode: JavascriptMode.unrestricted,
-          gestureNavigationEnabled: true,
-          allowsInlineMediaPlayback: true,
-          onWebViewCreated: (WebViewController webViewController) {
-            _controller.complete(webViewController);
-          },
-          onProgress: (int progress) {
-            // print('WebView is loading (progress : $progress%)');
-          },
-          javascriptChannels: <JavascriptChannel>{
-            _toasterJavascriptChannel(context),
-          },
-          navigationDelegate: (NavigationRequest request) {
-            if (request.url.startsWith('https://www.youtube.com/')) {
-              // print('blocking navigation to $request}');
-              return NavigationDecision.prevent;
-            }
-            // print('allowing navigation to $request');
-            return NavigationDecision.navigate;
-          },
-          onPageStarted: (String url) {
-            // print('Page started loading: $url');
-          },
-          onPageFinished: (String url) {
-            // print('Page finished loading: $url');
-          },
+        child: Column(
+          children: [
+            if (_progress < 1)
+              LinearProgressIndicator(value: _progress, minHeight: 1),
+            Expanded(
+              child: WebView(
+                initialUrl: 'https://flutter.dev',
+                javascriptMode: JavascriptMode.unrestricted,
+                gestureNavigationEnabled: true,
+                allowsInlineMediaPlayback: true,
+                onWebViewCreated: (WebViewController webViewController) {
+                  _controller.complete(webViewController);
+                },
+                onProgress: (int progress) {
+                  setState(() {
+                    _progress = progress / 100;
+                  });
+                  // print('WebView is loading (progress : $progress%)');
+                },
+                javascriptChannels: <JavascriptChannel>{
+                  _toasterJavascriptChannel(context),
+                },
+                navigationDelegate: (NavigationRequest request) {
+                  if (request.url.startsWith('https://www.youtube.com/')) {
+                    // print('blocking navigation to $request}');
+                    return NavigationDecision.prevent;
+                  }
+                  // print('allowing navigation to $request');
+                  return NavigationDecision.navigate;
+                },
+                onPageStarted: (String url) {
+                  // print('Page started loading: $url');
+                },
+                onPageFinished: (String url) {
+                  // print('Page finished loading: $url');
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
